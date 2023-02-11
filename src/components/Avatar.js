87524bd382aef10e4ba036124734cb618d2c30db
CharmCharm93 <caohoangmyduyen@gmail.com>
2023-02-11 19:45:38 +0700

    edit avatar part

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,89 +1,53 @@
 import React from "react";
 
-function Avatar({
-  body,
-  eyes,
-  hair,
-  mouth,
-  eyebrows,
-  hat,
-  glasses,
-  clothing1,
-  clothing2,
-  clothing3,
-  randomize,
-}) {
+function Avatar(props) {
   return (
-    <div className="avatar-wrapper">
-      <div className="avatar">
-        <img
-          src={`./character/body/${body}.png`}
-          alt=""
-          width={200}
-          style={{ zIndex: 0 }}
-        />
-
-        <img
-          src={`./character/eyes/${eyes}.png`}
-          alt=""
-          width={200}
-          style={{ zIndex: 7 }}
-        />
-
-        <img
-          src={`./character/hair/${hair}.png`}
-          alt=""
-          width={200}
-          style={{ zIndex: 1 }}
-        />
-        <img
-          src={`./character/mouths/${mouth}.png`}
-          alt=""
-          width={200}
-          style={{ zIndex: 2 }}
-        />
-        <img
-          src={`./character/eyebrows/${eyebrows}.png`}
-          alt=""
-          width={200}
-          style={{ zIndex: 1 }}
-        />
-        <img
-          src={`./character/accessories/hats/${hat}.png`}
-          alt=""
-          width={200}
-          style={{ zIndex: 10 }}
-        />
-        <img
-          src={`./character/accessories/glasses/${glasses}.png`}
-          alt=""
-          width={200}
-          style={{ zIndex: 1 }}
-        />
-        <img
-          src={`./character/clothes/layer_1/${clothing1}.png`}
-          alt=""
-          width={200}
-          style={{ zIndex: 3 }}
-        />
-        <img
-          src={`./character/clothes/layer_2/${clothing2}.png`}
-          alt=""
-          width={200}
-          style={{ zIndex: 4 }}
-        />
-        <img
-          src={`./character/clothes/layer_3/${clothing3}.png`}
-          alt=""
-          width={200}
-          style={{ zIndex: 5 }}
-        />
-      </div>
-      <div className="text-center">
-        <button className="randomBtn" onClick={() => randomize()}>
-          Randomize
-        </button>
-      </div>
+    <div>
+      <img
+        src={`./character/body/${props.body}.png`}
+        alt=""
+        style={{ zIndex: 0 }}
+      />
+      <img
+        src={`./character/eyes/${props.eyes}.png`}
+        alt=""
+        style={{ zIndex: 3 }}
+      />
+      <img
+        src={`./character/hair/${props.hair}.png`}
+        alt=""
+        style={{ zIndex: 2 }}
+      />
+      <img
+        src={`./character/mouths/${props.mouth}.png`}
+        alt=""
+        style={{ zIndex: 6 }}
+      />
+      <img
+        src={`./character/accessories/hats/${props.hat}.png`}
+        alt=""
+        style={{ zIndex: 3 }}
+      />
+      <img src={`./character/eyesbrow/${props.eyebrows}.png`} alt="" />
+      <img
+        src={`./character/accessories/glasses/${props.glasses}.png`}
+        alt=""
+      />
+      <img
+        src={`./character/clothes/layer_1/${props.clothing1}.png`}
+        alt=""
+        style={{ zIndex: 4 }}
+      />
+      <img
+        src={`./character/clothes/layer_2/${props.clothing2}.png`}
+        alt=""
+        style={{ zIndex: 5 }}
+      />
+      <img
+        src={`./character/clothes/layer_3/${props.clothing3}.png`}
+        alt=""
+        style={{ zIndex: 6 }}
+      />
     </div>
   );
 }
